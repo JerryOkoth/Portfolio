cc8ba244b326fb2abafd887daaef9e3f4d51ae68
NightHauler <jerryokoth06@gmail.com>
2024-09-18 02:32:46 +0300

    animated hero icons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Hamburger menu
+
 const menu= document.querySelector('nav ul');
 const menuBtn= document.querySelector('.menu-open');
 const closeBtn= document.querySelector('.menu-close');
@@ -9,3 +11,24 @@ menuBtn.addEventListener('click', () =>{
 closeBtn.addEventListener('click', () =>{
     menu.classList.remove('open')
 })
+
+
+
+
+// hero icons Animation
+
+window.addEventListener('load', () => {
+    const icons = document.querySelectorAll('.hero-icons a');
+    
+    // Add the fade-bounce class to each icon
+    icons.forEach((icon, index) => {
+        setTimeout(() => {
+            icon.classList.add('fade-bounce');
+        }, index * 200); // Add delay to make each icon animate sequentially
+    });
+});
+
+
+
+
+
